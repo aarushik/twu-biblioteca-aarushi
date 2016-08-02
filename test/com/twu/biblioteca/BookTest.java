3ce5e3735fbf3ec4ba73327778aec59d49064e86
Aarushi Kansal <aarushi.k@hotmail.com>
2016-08-03 00:28:42 +1000

    First test for book class - testing getter method for author

---
+++ test/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,25 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Aarushi on 2/08/2016.
+ */
+public class BookTest {
+
+    Book book;
+
+    @Before
+    public void setUp() {
+        book = new Book("Aarushi", 2016);
+    }
+
+    @Test
+    public void testGetAuthor() {
+        assertEquals("Aarushi", book.getAuthor());
+    }
+
+}
