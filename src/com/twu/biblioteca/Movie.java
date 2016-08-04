3cff5038730aff125a3015e55763a80b16b2c77e
Aarushi Kansal <aarushi.k@hotmail.com>
2016-08-04 21:42:31 +1000

    Movie tests and code

--- src/com/twu/biblioteca/Movie.java
+++ src/com/twu/biblioteca/Movie.java
@@ -4,4 +4,48 @@ package com.twu.biblioteca;
  * Created by Aarushi on 4/08/2016.
  */
 public class Movie {
+
+    private String name;
+    private String director;
+    private int year;
+    private int rating;
+
+    public Movie(String name, String director, int year, int rating) {
+        this.name = name;
+        this.director = director;
+        this.year = year;
+        this.rating = rating;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getRating() {
+        return rating;
+    }
 }
--- test/com/twu/biblioteca/MovieTest.java
+++ test/com/twu/biblioteca/MovieTest.java
@@ -1,7 +1,68 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Aarushi on 4/08/2016.
  */
 public class MovieTest {
+    Movie movie;
+    private static final String NAME = "Harry potter";
+    private static final String DIRECTOR = "David Yates";
+    private static final int YEAR = 2009;
+    private static final int RATING = 10;
+
+    @Before
+    public void setUp() {
+        movie = new Movie(NAME, DIRECTOR, YEAR, RATING);
+    }
+
+    @Test
+    public void getNameTest() {
+        assertEquals(NAME, movie.getName());
+    }
+
+    @Test
+    public void setNameTest() {
+        movie.setName("Deathly Hallows");
+        assertEquals("Deathly Hallows", movie.getName());
+    }
+
+    @Test
+    public void getDirectorTest() {
+        assertEquals(DIRECTOR, movie.getDirector());
+    }
+
+    @Test
+    public void setDirectorTest() {
+        movie.setDirector("Chris Columbus");
+        assertEquals("Chris Columbus", movie.getDirector());
+    }
+
+    @Test
+    public void getYearTest() {
+        assertEquals(YEAR, movie.getYear());
+    }
+
+    @Test
+    public void setYearTest() {
+        movie.setYear(2000);
+        assertEquals(2000, movie.getYear());
+    }
+
+    @Test
+    public void getRatingTest() {
+        assertEquals(RATING,movie.getRating());
+    }
+
+    @Test
+    public void setRatingTest() {
+        movie.setRating(2);
+        assertEquals(2, movie.getRating());
+    }
+
+
 }
