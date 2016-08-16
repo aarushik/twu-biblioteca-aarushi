64602f3d2300ce7541a44f265e378de8387f59c0
Aaurshi Kansal <akansal@thoughtworks.com>
2016-08-16 16:29:14 +1000

    Added a credential class to handle authentication and tests for authentication

--- src/com/twu/biblioteca/Credential.java
+++ src/com/twu/biblioteca/Credential.java
@@ -1,7 +1,33 @@
 package com.twu.biblioteca;
 
+import javax.security.auth.login.CredentialNotFoundException;
+import java.util.ArrayList;
+
 /**
  * Created by akansal on 16/08/2016.
  */
 public class Credential {
+
+    private Library library;
+
+
+    public Credential(Library library) {
+        this.library = library;
+
+    }
+
+    public Boolean authenticate(String libraryId, String password) {
+        Boolean authenticated = false;
+
+        User user = library.getUser(libraryId);
+
+        if(user != null && user.getPassword().equals(password)) {
+            authenticated = true;
+        }
+
+        return authenticated;
+    }
+
+
+
 }
--- test/com/twu/biblioteca/CredentialTest.java
+++ test/com/twu/biblioteca/CredentialTest.java
@@ -1,7 +1,35 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by akansal on 16/08/2016.
  */
 public class CredentialTest {
+
+    Credential creds;
+    Library library;
+
+    @Before
+    public void setUp() {
+        library = TestHelper.populateLibrary();
+        creds = new Credential(library);
+    }
+
+    @Test
+    public void itShouldReturnFalseWhenInvalidCredentials() {
+        assertEquals(false,creds.authenticate("111-1113", "password"));
+    }
+
+    @Test
+    public void itShouldReturnTrueWhenValidCredentials() {
+        assertEquals(true, creds.authenticate("111-1111", "password"));
+    }
+
+
 }
