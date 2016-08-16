a02d1c220afccce17c511f29e7c94e6da4def0a5
Aaurshi Kansal <akansal@thoughtworks.com>
2016-08-16 16:12:12 +1000

    Created a helper class to help create and populate Library when testing

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -40,7 +40,7 @@ public class LibraryTest {
         when(user.getLibraryNumber()).thenReturn("111-1111");
         when(user.getPassword()).thenReturn("password");
 
-        populateLibrary();
+        library = TestHelper.populateLibrary();
 
 
         console = System.out;
@@ -112,23 +112,6 @@ public class LibraryTest {
         assertEquals(null, library.getUser("invalidID"));
     }
 
-    private void populateLibrary() {
-        ArrayList<Article> articles = new ArrayList<Article>();
-        ArrayList<User> users = new ArrayList<User>();
 
-        Book javaBook = new Book("Aarushi", 2016, "Intro to Java", true);
-
-        Movie movie1 = new Movie("Harry Potter", "David Yates", 2009, 10, true);
-
-        User user = new User("111-1111", "password", "Aarushi Kansal", false, "user@email.com", "0430512710");
-        users.add(user);
-
-        articles.add(javaBook);
-        articles.add(movie1);
-
-        library = new Library(articles, users);
-
-
-    }
 
 }
---
+++ test/com/twu/biblioteca/TestHelper.java
@@ -0,0 +1,33 @@
+package com.twu.biblioteca;
+
+import java.util.ArrayList;
+
+/**
+ * Created by akansal on 16/08/2016.
+ */
+public class TestHelper {
+    static Library library;
+
+    private TestHelper() {
+
+    }
+
+    public static Library populateLibrary() {
+        ArrayList<Article> articles = new ArrayList<Article>();
+        ArrayList<User> users = new ArrayList<User>();
+
+        Book javaBook = new Book("Aarushi", 2016, "Intro to Java", true);
+
+        Movie movie1 = new Movie("Harry Potter", "David Yates", 2009, 10, true);
+
+        User user = new User("111-1111", "password", "Aarushi Kansal", false, "user@email.com", "0430512710");
+        users.add(user);
+
+        articles.add(javaBook);
+        articles.add(movie1);
+
+        library = new Library(articles, users);
+
+        return library;
+    }
+}
